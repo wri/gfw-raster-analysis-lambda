ff8f744d620066b15c26f5f081755c751419bfd0
sgibbes <sgibbes@wri.org>
2018-01-18 14:53:07 -0500

    first commit. check if user-drawn poly is within data extent of rasters

---
+++ utilities/check_extent.py
@@ -0,0 +1,32 @@
+import boto3
+import ast
+from shapely.geometry import Polygon, MultiPolygon
+import json
+
+
+def check_extent(user_poly, raster):
+    s3 = boto3.resource('s3')
+
+    bucket= s3.Bucket('palm-risk-poc')
+
+    # read context of the index file
+    d = bucket.Object('data/{}/index.geojson'.format(raster)).get()['Body'].read()
+
+    # get contents from string to dictionary
+    d = json.loads(d)
+
+    # get index geom
+    index_geom = d['features'][0]['geometry']['coordinates'][0]
+    poly_list = [Polygon(x['geometry']['coordinates'][0]) for x in d['features']]
+    
+    # read in indexgeom as shapely multipolygon
+    index_geom = MultiPolygon(poly_list)
+
+    # get user geom
+    user_geom = user_poly['features'][0]['geometry']['coordinates'][0]
+    
+    # read in user geom as shapely geometry
+    user_poly = Polygon(user_geom)
+
+    # check if polygons intersect
+    return user_poly.intersects(index_geom)
