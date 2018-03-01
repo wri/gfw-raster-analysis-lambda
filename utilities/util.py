424dda86f9b637fb7ae863160c17a849d32f0d07
sgibbes <sgibbes@wri.org>
2018-03-01 16:39:50 -0500

    for glad alerts, cleaned up validate params and allow for handling of period

--- utilities/util.py
+++ utilities/util.py
@@ -1,6 +1,7 @@
 import json
 import datetime
 from collections import defaultdict
+import sys
 
 from shapely.geometry import shape
 
@@ -56,7 +57,7 @@ def create_resp_dict(date_dict):
     return resp_dict
 
 
-def unpack_glad_histogram(stats, hist_type):
+def unpack_glad_histogram(stats, hist_type, period):
 
 	date_dict = {}
 
@@ -73,7 +74,11 @@ def unpack_glad_histogram(stats, hist_type):
 	    except KeyError:
 		    date_dict[alert_date] = count # if date doesn't exist, create it, set equal to count
 
-        resp_dict = create_resp_dict(date_dict)
+        # filter dates by period
+        start_date, end_date = period_to_dates(period)
+        filtered_by_period = {alert_date : count for alert_date, count in date_dict.iteritems() if start_date < alert_date < end_date}
+
+        resp_dict = create_resp_dict(filtered_by_period)
 
 	return resp_dict.get(hist_type, {'all': resp_dict})
 
@@ -100,3 +105,67 @@ def grouped_and_to_rows(keys, vals, agg_type):
 
     return final_list
 
+def set_default_period():
+    today = datetime.datetime.now().date()
+    today = today.strftime('%Y-%m-%d')
+
+    return "2015-01-01,{}".format(today)
+
+def validate_glad_params(event):
+
+    params = event.get('queryStringParameters')
+
+    if not params:
+        params = {}
+
+    # if period isn't supplied, set default
+    period = params.get('period', set_default_period())
+
+    params['period'] = period
+
+    # make sure the supplied period is in the right format, dates make sense
+    try:
+        check_dates(period)
+    except ValueError, e:
+        raise ValueError(e)
+
+
+    agg_values = params.get('aggregate_values', False)
+    agg_by = params.get('aggregate_by')
+
+    if agg_values in ['true', 'TRUE', 'True', True]:
+        agg_values = True
+
+    agg_list = ['day', 'week', 'month', 'year', 'quarter', 'all']
+
+    if agg_by not in agg_list or agg_values != True:
+        msg = 'For this batch service, aggregate_values must be True, and ' \
+              'aggregate_by must be in {}'.format(', '.join(agg_list))
+        raise ValueError(msg)
+
+
+    return params
+
+
+def check_dates(period):
+
+    try:
+        start_date, end_date = period_to_dates(period)
+    except ValueError:
+        raise ValueError('period must be formatted as YYYY-mm-dd,YYYY-mm-dd')
+
+    if start_date > end_date:
+        raise ValueError('Start date must be <= end date')
+
+    earliest_date = datetime.datetime.strptime('2015-01-01', '%Y-%m-%d')
+    if start_date < earliest_date:
+        raise ValueError('Start date must be later than Jan 1, 2015')
+
+
+def period_to_dates(period):
+
+    start_date, end_date = period.split(',')
+    start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+
+    return start_date, end_date
