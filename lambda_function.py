6b1b474ddfff893d9b8ab4be91b972f76b400ff4
Thomas Maschler <tmaschler@wri.org>
2019-05-31 15:33:57 +0200

    debug, dockerignore
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- lambda_function.py
+++ lambda_function.py
@@ -2,16 +2,17 @@ from raster_analysis import geoprocessing
 from raster_analysis.utilities import grid_id
 from shapely.geometry import shape
 
+# import sys
+# import json
 
 BASE_URL = "/vsis3/gfw-files/2018_update/{raster_id}/{tile_id}.tif"
 
 
 def lambda_handler(event, context):
-
-    raster_ids = event.raster_ids
-    analysis = event.analsis_id
-    geometry = shape(event.geometry)
-    threshold = event.threshold
+    raster_ids = event["raster_ids"]
+    analysis = event["analysis"]
+    geometry = shape(event["geometry"])
+    threshold = event["threshold"]
 
     centroid = geometry.centroid
 
@@ -23,8 +24,12 @@ def lambda_handler(event, context):
         rasters.append(raster)
 
     if analysis == "area_sum":
-        result = geoprocessing.sum_analysis(geoprocessing, rasters, threshold=threshold, area=True)
+        result = geoprocessing.sum_analysis(geometry, *rasters, threshold=threshold, area=True)
     else:
         result = {"error": "analysis type unknown"}
 
     return result
+
+# if __name__ == "__main__":
+#     #"{\"raster_ids\":[\"loss\", \"tcd_2000\", \"tcd_2010\", \"wdpa\"], \"analysis\":\"area_sum\", \"threshold\":30, \"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[9.0,4.1],[9.1,4.1],[9.1,4.2],[9.0,4.2],[9.0,4.1]]]}}"
+#     print(lambda_handler(json.loads(sys.argv[1]), None))
