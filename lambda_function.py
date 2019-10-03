43a381179dbf198e1613a7192fc3d7e598183880
Thomas Maschler <tmaschler@wri.org>
2019-10-03 13:47:30 -0400

    switch to lambda_decorators for validation and serialization add additional pre-commit hooks add build scripts for package.zip
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- lambda_function.py
+++ lambda_function.py
@@ -1,53 +1,16 @@
+from lambda_decorators import json_http_resp, json_schema_validator
+from shapely.geometry import shape
+
 from raster_analysis import geoprocessing
 from raster_analysis.geoprocessing import Filter
-from shapely.geometry import shape
-import traceback
-import logging
-
-import sys
-import json
+from raster_analysis.schemas import SCHEMA
 
 BASE_URL = "/vsis3/gfw-files/2018_update/{raster_id}/{tile_id}.tif"
 
 
-def serialize(func):
-    def wrapper(*args, **kwargs):
-        try:
-            body = func(*args, **kwargs)
-            result = {
-                "statusCode": 200,
-                "headers": {"Content-Type": "application/json"},
-                "body": body,
-            }
-        except ValueError as e:
-            return {
-                "statusCode": 400,
-                "headers": {"Content-Type": "application/json"},
-                "body": {"message": str(e)},
-            }
-        except Exception:
-            logging.error("[ERROR][RasterAnalysis] " + traceback.format_exc())
-            result = {
-                "statusCode": 500,
-                "headers": {"Content-Type": "application/json"},
-                "body": {"message": "Internal Server Error. Please check logs."},
-            }
-
-        return json.dumps(result)
-
-    return wrapper
-
-
-@serialize
+@json_schema_validator(request_schema=SCHEMA)
+@json_http_resp
 def lambda_handler(event, context):
-    missing_required_params = []
-    if "analysis_raster_id" not in event:
-        missing_required_params.append("analysis_raster_id")
-    elif "geometry" not in event:
-        missing_required_params.append("geometry")
-
-    if missing_required_params:
-        raise ValueError("Missing parameters: " + ", ".join(missing_required_params))
 
     analysis_raster_id = event["analysis_raster_id"]
     contextual_raster_ids = (
@@ -58,9 +21,6 @@ def lambda_handler(event, context):
     )
     analyses = event["analyses"] if "analyses" in event else ["count", "area"]
     geometry = shape(event["geometry"])
-
-    if any(analysis not in ["area", "sum", "count"] for analysis in analyses):
-        raise ValueError("Unknown analysis: " + analyses)
 
     filters = [Filter(**f) for f in event["filters"]] if "filters" in event else []
 
@@ -76,4 +36,20 @@ def lambda_handler(event, context):
 
 if __name__ == "__main__":
     # "{\"analysis_raster_id\":\"loss\", \"contextual_raster_ids\":[\"wdpa\"], \"aggregate_raster_ids\":[\"tcd_2000\", \"tcd_2010\"], \"analysis\":\"sum\", \"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[9.0,4.1],[9.1,4.1],[9.1,4.2],[9.0,4.2],[9.0,4.1]]]},\"filters\":[{\"raster_id\":\"tcd_2000\",\"threshold\":30}]}"), None))
-    print(lambda_handler(json.loads(sys.argv[1]), None))
+    print(
+        lambda_handler(
+            {
+                "analysis_raster_id": "loss",
+                "contextual_raster_ids": ["wdpa"],
+                "analyses": ["count", "area"],
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [
+                        [[9, 4.1], [9.1, 4.1], [9.1, 4.2], [9, 4.2], [9, 4.1]]
+                    ],
+                },
+                "filters": [{"raster_id": "tcd_2000", "threshold": 30}],
+            },
+            None,
+        )
+    )
