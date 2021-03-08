dadf43179ba2ab5806e8795aa4f9e9be16996719
justin.terry@wri.org <jt@c02xrkdcjg5h.myfiosgateway.com>
2021-03-08 15:54:19 -0500

    Fix dockerfile

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="gfw_raster-analysis-lambda",
-    version="1.0.0",
+    version="2.0.0",
     description="Lambda function to run serverless on the fly raster analysis",
-    packages=["raster_analysis", "raster_analysis.layer"],
+    packages=["raster_analysis"],
     author="Justin Terry/Thomas Maschler",
     license="MIT",
     install_requires=["aws-xray-sdk", "requests", "geobuf"],
