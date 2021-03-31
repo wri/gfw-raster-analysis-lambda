9ecc34dbdf9a993c7b950e0cc9651dc42535e860
justin.terry@wri.org <jt@c02xrkdcjg5h.myfiosgateway.com>
2021-03-31 11:35:40 -0400

    Only log full results as debug

--- lambdas/raster_analysis/src/lambda_function.py
+++ lambdas/raster_analysis/src/lambda_function.py
@@ -31,7 +31,7 @@ def handler(event, context):
         query_executor = QueryExecutor(query, data_cube)
         results: DataFrame = query_executor.execute()
 
-        LOGGER.info(f"Ran analysis with results: {results}")
+        LOGGER.debug(f"Ran analysis with results: {results}")
         results_store.save_result(results, context.aws_request_id)
     except Exception as e:
         results_store = AnalysisResultsStore(event["analysis_id"])
@@ -39,4 +39,3 @@ def handler(event, context):
 
         LOGGER.exception(e)
         raise Exception(f"Internal Server Error <{context.aws_request_id}>")
-
