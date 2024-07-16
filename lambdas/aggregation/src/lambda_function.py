fd0ad1751e0c99f55b617ae0f454603b83bfc5ee
Solomon Negusse <solomon.negusse@wri.org>
2024-07-16 12:36:56 +0300

    return jsend format response; return list of failed geometries; clean up response not including jsend from individual lambda runs

--- lambdas/aggregation/src/lambda_function.py
+++ lambdas/aggregation/src/lambda_function.py
@@ -24,6 +24,7 @@ def handler(event, context):
         LOGGER.info("manifest file", manifest)
 
         combined_data = []
+        failed_geometries = []
         for result_record in manifest["ResultFiles"]["SUCCEEDED"]:
             response = s3_client().get_object(
                 Bucket=results_meta["Bucket"], Key=result_record["Key"]
@@ -31,23 +32,36 @@ def handler(event, context):
             results = json.loads(response["Body"].read().decode("utf-8"))
             for geom_result in results:
                 result = json.loads(geom_result["Output"])
-
-                combined_data.append(result)
+                if result["status"] == "success":
+                    combined_data.append(
+                        {"result": result["data"], "geometry_id": result["fid"]}
+                    )
+                else:
+                    failed_geometries.append(result["fid"])
 
         LOGGER.info("Successfully aggregated results")
 
         results_prefix = "/".join(results_meta["Key"].split("/")[:-1])
         results_key = f"{results_prefix}/analysis_results.json"
+        failed_list_key = f"{results_prefix}/failed_geometries.json"
 
         s3_client().put_object(
             Bucket=results_meta["Bucket"],
             Key=results_key,
             Body=json.dumps(combined_data),
         )
+        s3_client().put_object(
+            Bucket=results_meta["Bucket"],
+            Key=failed_list_key,
+            Body=json.dumps(failed_geometries),
+        )
 
         return {
             "status": "success",
-            "results_uri": f"s3://{results_meta['Bucket']}/{results_key}",
+            "data": {
+                "analysis_results": f"s3://{results_meta['Bucket']}/{results_key}",
+                "failed_geometries": f"s3://{results_meta['Bucket']}/{failed_list_key}",
+            },
         }
     except QueryParseException as e:
         return {"status": "failed", "message": str(e)}
