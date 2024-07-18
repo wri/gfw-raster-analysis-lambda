310ce3ef987682ab236c6f7b362aa9bedc9e8d6a
Solomon Negusse <solomon.negusse@wri.org>
2024-07-18 15:10:22 +0300

    revert failure tests

--- lambdas/list_tiled_analysis/src/lambda_function.py
+++ lambdas/list_tiled_analysis/src/lambda_function.py
@@ -12,39 +12,38 @@ patch(["boto3"])
 
 @xray_recorder.capture("Tiled Analysis")
 def handler(event, context):
-    raise NameError("foo")
-    # try:
-    #     LOGGER.info(f"Running analysis with parameters: {event}")
+    try:
+        LOGGER.info(f"Running analysis with parameters: {event}")
 
-    #     query = event["query"]
-    #     fid = event.get("fid", None)
-    #     geojson = mapping(loads(bytes.fromhex(event["geometry"])))
-    #     data_environment = DataEnvironment(layers=event["data_environment"])
+        query = event["query"]
+        fid = event.get("fid", None)
+        geojson = mapping(loads(bytes.fromhex(event["geometry"])))
+        data_environment = DataEnvironment(layers=event["data_environment"])
 
-    #     LOGGER.info(f"Executing query: {query}")
-    #     LOGGER.info(f"On geometry: {geojson}")
+        LOGGER.info(f"Executing query: {query}")
+        LOGGER.info(f"On geometry: {geojson}")
 
-    #     tiler = AnalysisTiler(query, geojson, context.aws_request_id, data_environment)
-    #     tiler.execute()
+        tiler = AnalysisTiler(query, geojson, context.aws_request_id, data_environment)
+        tiler.execute()
 
-    #     results = tiler.result_as_dict()
+        results = tiler.result_as_dict()
 
-    #     LOGGER.info("Successfully merged tiled results")
-    #     response = {"status": "success", "data": results}
-    #     if fid:
-    #         response["fid"] = fid
+        LOGGER.info("Successfully merged tiled results")
+        response = {"status": "success", "data": results}
+        if fid:
+            response["fid"] = fid
 
-    #     return response
-    # except QueryParseException as e:
-    #     response = {"status": "failed", "message": str(e)}
-    #     if fid:
-    #         response["fid"] = fid
+        return response
+    except QueryParseException as e:
+        response = {"status": "failed", "message": str(e)}
+        if fid:
+            response["fid"] = fid
 
-    #     return response
-    # except Exception as e:
-    #     LOGGER.exception(e)
-    #     response = {"status": "error", "message": str(e)}
-    #     if fid:
-    #         response["fid"] = fid
+        return response
+    except Exception as e:
+        LOGGER.exception(e)
+        response = {"status": "error", "message": str(e)}
+        if fid:
+            response["fid"] = fid
 
-    #     return response
+        return response
