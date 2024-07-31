17395a4cb0ead7b7a778fd430539dd3e200d5207
Justin Terry <justin.terry@wri.org>
2024-07-31 11:00:20 -0700

    Add state machine ARN to output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "raster_analysis_lambda_name" {
 output "raster_analysis_lambda_arn" {
   value = aws_lambda_function.tiled_raster_analysis.arn
 }
+
+output "raster_analysis_state_machine_arn" {
+  value = aws_sfn_state_machine.process_list.arn
+}
