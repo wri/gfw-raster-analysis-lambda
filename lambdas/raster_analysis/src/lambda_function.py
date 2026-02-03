8b0e6639e20fa7a17af8a258206d4e4a6266a312
Daniel Mannarino <daniel.mannarino@gmail.com>
2026-02-03 18:48:06 -0500

    raster analysis lambda: Make it possible to disable CodeGuru at run time; introduce indirection for invoking lambdas and storing results while benchmarking; factor out compute function to allow microbenchmarking to skip IO cost of saving results

--- lambdas/raster_analysis/src/lambda_function.py
+++ lambdas/raster_analysis/src/lambda_function.py
@@ -1,4 +1,5 @@
-from codeguru_profiler_agent import with_lambda_profiler
+import os
+
 from pandas import DataFrame
 
 from raster_analysis.data_cube import DataCube
@@ -9,43 +10,55 @@ from raster_analysis.query import Query
 from raster_analysis.query_executor import QueryExecutor
 from raster_analysis.results_store import AnalysisResultsStore, ResultStatus
 
+try:
+    if os.getenv("DISABLE_CODEGURU", "").lower() in ("1", "true", "yes"):
+        raise ImportError()
+    from codeguru_profiler_agent import with_lambda_profiler
+except ImportError:
+    def with_lambda_profiler(*args, **kwargs):
+        def deco(fn):
+            return fn
+        return deco
+
 
 @with_lambda_profiler(profiling_group_name="raster_analysis_default_profiler")
 def handler(event, context):
+    results_store = event.get("__bench_results_store__") or AnalysisResultsStore()
     try:
         LOGGER.info(f"Running analysis with parameters: {event}")
-        results_store = AnalysisResultsStore()
-
-        if "geometry" in event:
-            source_geom = event["geometry"]
-            is_encoded = False
-        elif "encoded_geometry" in event:
-            source_geom = event["encoded_geometry"]
-            is_encoded = True
-        else:
-            raise KeyError("No valid geometry field")
-
-        tile_geojson = event.get("tile", None)
-        geom_tile = GeometryTile(source_geom, tile_geojson, is_encoded)
-
-        if not geom_tile.geom:
-            LOGGER.info(f"Geometry for tile {context.aws_request_id} is empty.")
-            results_store.save_result(DataFrame(), context.aws_request_id)
-            return {}
-
-        data_environment = DataEnvironment(layers=event["environment"])
-        query = Query(event["query"], data_environment)
-
-        data_cube = DataCube(geom_tile.geom, geom_tile.tile, query)
-
-        query_executor = QueryExecutor(query, data_cube)
-        results: DataFrame = query_executor.execute()
+        results: DataFrame = compute(event, context, results_store)
 
         LOGGER.debug(f"Ran analysis with results: {results.head(100)}")
         results_store.save_result(results, event["cache_id"])
     except Exception as e:
         LOGGER.exception(e)
-
-        results_store = AnalysisResultsStore()
         results_store.save_status(event["cache_id"], ResultStatus.error, 0, str(e))
         raise e
+
+
+def compute(event, context, results_store) -> DataFrame:
+    if "geometry" in event:
+        source_geom = event["geometry"]
+        is_encoded = False
+    elif "encoded_geometry" in event:
+        source_geom = event["encoded_geometry"]
+        is_encoded = True
+    else:
+        raise KeyError("No valid geometry field")
+
+    tile_geojson = event.get("tile", None)
+    geom_tile = GeometryTile(source_geom, tile_geojson, is_encoded)
+
+    if not geom_tile.geom:
+        LOGGER.info(f"Geometry for tile {context.aws_request_id} is empty.")
+        results_store.save_result(DataFrame(), context.aws_request_id)
+        return {}
+
+    data_environment = DataEnvironment(layers=event["environment"])
+    query = Query(event["query"], data_environment)
+
+    data_cube = DataCube(geom_tile.geom, geom_tile.tile, query)
+
+    query_executor = QueryExecutor(query, data_cube)
+    results: DataFrame = query_executor.execute()
+    return results
